df3c511b9e06619b97f0aac64cb6d2ad95e7bc6f
chickenstu <manuel.tausch@gmx.de>
2016-11-19 12:34:34 -0800

    Add files via upload
    Why is the QWidget (class mainChild) interface being merged with it's parent interface (class mainWin) ? The child dialog should appear in a separate window.
    I must be doing something fundamentally wrong when setting up parent/child relationships between a 'QMainWindow' class and a 'QWidget' class.

---
+++ qmainwindow_issue.py
@@ -0,0 +1,35 @@
+from PySide.QtGui import QMainWindow, QApplication, QWidget, QPushButton
+from PySide.QtCore import *
+import sys
+
+
+class mainChild(QWidget):
+	def __init__(self, parent=None):
+		super(mainChild, self).__init__(parent)
+		self.setWindowTitle("main window that's right")
+		self.setGeometry(00, 100, 400, 400)
+		self.button = QPushButton("I Should appear in separate Window!!", self)
+		self.show()
+
+
+class mainWin(QMainWindow):
+	def __init__(self):
+		super(mainWin, self).__init__()
+		self.setWindowTitle("main window that's right")
+		self.setGeometry(100, 100, 300, 300)
+		self.myChild = None
+		self.statusBar().showMessage("test")
+		self.button = QPushButton("child dialog", self)
+		self.button.clicked.connect(self.openChild)
+		self.show()
+
+	def openChild(self):
+		self.myChild = mainChild(self)
+
+
+if __name__ == "__main__":
+	print "aight"
+	app = QApplication(sys.argv)
+	print "yes"
+	test = mainWin()
+	sys.exit(app.exec_())
